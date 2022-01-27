6041ccc90ed464aefa8b33952159b6f1155c3361
embeddedart <khokharbrijesh@gmail.com>
2022-01-27 19:13:36 +0530

    pattern committed

---
+++ StarPattern.java
@@ -0,0 +1,20 @@
+package com.example_Java;
+
+/**
+ * Practical 5 : program to demonstrate star pattern using for loop
+ * @author Mitul
+ * @version 1.0
+ * @since 27-01-2022
+ */
+public class StarPattern {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) { // Loop for row
+            for (int j = 0; j <= i; j++) { // Loop for column
+                System.out.print("*"); // Print stars
+            }
+            System.out.println(); // For new line
+        }
+    }
+
+}
