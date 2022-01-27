b40d7435e9fd8e43dd61ff9b972b4d48476f0648
embeddedart <khokharbrijesh@gmail.com>
2022-01-27 19:06:35 +0530

    Age committed

--- ValidateAge.java
+++ ValidateAge.java
@@ -2,17 +2,17 @@ package com.example_Java;
 import java.util.Scanner;
 
 /**
- * Practical 2 : program to demonstrate the use of if/else statement. (E.g Define an 'age' variable and check your age)
+ * Practical 2 : program to demonstrate if/else statement
  * @author Mitul
  * @version 1.0
  * @since 27-01-2022
  */
-public class Age {
+public class ValidateAge {
 
     public static void main(String[] args) {
         System.out.println("Enter your age: "); // Print Enter your age:
-        Scanner scanner=new Scanner(System.in);
-        int age=scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        int age = scanner.nextInt();
 
         if (age >= 18) {
             System.out.println("You are eligible to Vote."); // Print You are eligible to Vote.
