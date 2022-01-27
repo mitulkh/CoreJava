b9c76ff361a3209c6f55b5001b457449ae890161
embeddedart <khokharbrijesh@gmail.com>
2022-01-27 12:39:32 +0530

    hello world committed

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+package com.example_Java;
+
+/**
+ * Practical 1 : program to print hello world
+ * @author Mitul
+ * @version 1.0
+ * @since 27-01-2022
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!"); // Print Hello World!!!
+    }
+
+}
