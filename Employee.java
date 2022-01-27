b0cb0e362deca16bef5396ad05467f66e597383d
embeddedart <khokharbrijesh@gmail.com>
2022-01-27 19:19:24 +0530

    employee committed

---
+++ Employee.java
@@ -0,0 +1,32 @@
+package com.example_Java;
+
+/**
+ * Practical 11 : program to use this/super variable
+ * @author Mitul
+ * @version 1.0
+ * @since 27-01-2022
+ */
+class Person{ // Parent class
+    int id;
+    String name;
+    Person(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+}
+
+public class Employee extends Person { // child class
+    float salary;
+    Employee(int id, String name, float salary){
+        super(id,name); // Super keyword used for accessing parent class variable
+        this.salary = salary; // This keyword used for current class instance variable
+        System.out.println("Id:"+id);
+        System.out.println("Name:"+name);
+        System.out.println("Salary:"+salary);
+    }
+
+    public static void main(String[] args) {
+        Employee employee = new Employee(102,"James",10000);
+    }
+
+}
