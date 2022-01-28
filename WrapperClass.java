9a85f248528bfb103fb8172f3ded3ac5e4574813
embeddedart <khokharbrijesh@gmail.com>
2022-01-28 12:59:49 +0530

    28-01-22 committed

--- WrapperClass.java
+++ WrapperClass.java
@@ -9,11 +9,11 @@ package com.exampleJava;
 public class WrapperClass {
 
     public static void main(String[] args) {
-        int num1 = 23;
+        int num1 = 512;
         Integer object1 = Integer.valueOf(num1);
-        int num2 = 12;
+        int num2 = 512;
         Integer object2 = Integer.valueOf(num2);
-        if (object1 == object2) {
+        if (object1.equals(object2)) {
             System.out.println("Same");
         } else {
             System.out.println("Different");
